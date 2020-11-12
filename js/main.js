554746c3b0edeef66bf678b844999b2bd43497df
Will Locklier <will.locklier@gmail.com>
2020-11-12 10:55:40 -0500

    [counter] Add annotations

--- js/main.js
+++ js/main.js
@@ -1,31 +1,16 @@
+//Global Variables
 const countDisplay = document.getElementById('count-display');
 const plusButton = document.getElementById('plus-sign');
 const inputValue = document.getElementById('input-value');
 const minusButton = document.getElementById('minus-sign');
 let counter = 0;
 
+
+// Event Listeners
 plusButton.addEventListener('click', addInputToCounter);
 minusButton.addEventListener('click', removeInputfromCounter);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Funcitons
-
 function addInputToCounter() {
     counter = counter + parseInt(inputValue.value)
     console.log(counter)
